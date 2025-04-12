cadd8d6f156eb63290d65afb57f06ce34ead8074
Mike Solomon <mike.solomon@coinbase.com>
2025-04-12 18:45:31 -0400

    add new contracts

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -4,9 +4,9 @@ import Mailbox from "../svg/Mailbox";
 import Planet from "../svg/Planet";
 
 export const USDC = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913';
-export const CONTRACT: `0x${string}` = '0x3ff0ef4d24919e03b5a650f2356bd632c59ef9f6';
-export const REVIEW_CONTRACT: `0x${string}` = '0x46270a5549d55898fbbe102f5560313903e7576e';
-export const SUGGESTED_PAYMENT_AMOUNTS_CONTRACT: `0x${string}` = '0xf0af2c550b51f3e4fe1b7dcfd4ac8a7093f54b94';
+export const CONTRACT: `0x${string}` = '0xc55cf49f1b8f69e584d9a5f8017c28dec0f28479';
+export const REVIEW_CONTRACT: `0x${string}` = '0x0be22550336c98a0c37e1f93f93fe4469f9abbaa';
+export const SUGGESTED_PAYMENT_AMOUNTS_CONTRACT: `0x${string}` = '0xc370cf4a0a64bd3d8cc172f89a12bd1c12c73af5';
 export const CHAIN = {
   ...base,
   rpc: `https://api.developer.coinbase.com/rpc/v1/base/${process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}`,
