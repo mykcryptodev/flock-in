e9af0363e3ddac1d61879d795ed58ec0635aa7af
Mike Solomon <mike.solomon@coinbase.com>
2025-04-22 23:05:34 -0400

    bump tw

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -7,10 +7,7 @@ export const USDC = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913';
 export const CONTRACT: `0x${string}` = '0x298688df47fa6ab1e4479f60474d16ad7c37d024';
 export const REVIEW_CONTRACT: `0x${string}` = '0x3fe1eb3912ea77230876eebd133a6a3467e2f93b';
 export const SUGGESTED_PAYMENT_AMOUNTS_CONTRACT: `0x${string}` = '0x72503a680c540fb3500561aaa288deb939a24b20';
-export const CHAIN = {
-  ...base,
-  rpc: `https://api.developer.coinbase.com/rpc/v1/base/${process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}`,
-}
+export const CHAIN = base;
 
 export const TABS: {
   name: string;
